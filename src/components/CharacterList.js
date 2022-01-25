9acfd08d1b96e7548c20b75352e5097cb1b23cfe
ElenaFernandez <elena.fernandez92@gmail.com>
2022-01-25 19:41:55 +0100

    add changes Scss

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,9 +1,9 @@
 import Character from './Character';
-
+import List from '../styles/layout/List.scss';
 function CharacterList(props) {
   const characterElement = props.character.map((character, index) => {
     return (
-      <li key={index}>
+      <li key={index} className="list_character">
         <Character data={character} />
       </li>
     );
@@ -11,7 +11,7 @@ function CharacterList(props) {
 
   return (
     <section>
-      <ul>{characterElement}</ul>
+      <ul className="list">{characterElement}</ul>
     </section>
   );
 }

2fe104e182b3818eadbcec7c9ad21e7fb808577d
ElenaFernandez <elena.fernandez92@gmail.com>
2022-01-26 18:25:25 +0100

    add scss structure list

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import CharacterCard from '../styles/layout/CharacterCard.scss';
 
 function Character(props) {
   return (
@@ -6,12 +7,18 @@ function Character(props) {
       <Link to={`/character/${props.data.id}`}>
         <img
           className="image"
-          src={props.data.image ? props.data.image : ('https://lafrikileria.com/71944-thickbox_default/funko-pop-gigante-harry-potter-y-hedwig-45-cm-xxl.jpg') }
+          src={
+            props.data.image
+              ? props.data.image
+              : 'https://lafrikileria.com/71944-thickbox_default/funko-pop-gigante-harry-potter-y-hedwig-45-cm-xxl.jpg'
+          }
           alt={`Foto de ${props.data.name}`}
           title={`Foto de ${props.data.name}`}
         />
-        <h4>{props.data.name}</h4>
-        <p>{props.data.species}</p>
+        <div className="container-character__title">
+          <h4>{props.data.name}</h4>
+          <p>{props.data.species}</p>
+        </div>
       </Link>
     </div>
   );
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -4,15 +4,15 @@ import '../styles/layout/List.scss';
 function CharacterList(props) {
   const characterElement = props.character.map((character, index) => {
     return (
-      <li key={index} className="list_character">
+      <li key={index} className="container-list__character">
         <Character data={character} />
       </li>
     );
   });
 
   return (
-    <div className="container-list">
-      <ul className="list">{characterElement}</ul>
+    <div>
+      <ul className="container-list">{characterElement}</ul>
     </div>
   );
 }

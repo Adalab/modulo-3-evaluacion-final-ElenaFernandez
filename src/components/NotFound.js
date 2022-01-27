4ac6b3c65a873aca7b9680a6e544bfac523b23de
ElenaFernandez <elena.fernandez92@gmail.com>
2022-01-27 11:22:13 +0100

    update changes scss

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,14 @@
-import   '../styles/layout/NotFoundText.scss'
-function NotFound(){
-    return(
-        <div className="text">
-            Oops! No se ha encontrado ningun personaje, vuelve a buscar.
-        </div>
-    )
+import '../styles/layout/NotFoundText.scss';
+import Footer from './Footer';
+function NotFound() {
+  return (
+    <>
+      <div className="text">
+        Oops! No se ha encontrado ningun personaje, vuelve a buscar.
+      </div>
+      <Footer/>
+    </>
+  );
 }
 
-export default NotFound
+export default NotFound;
